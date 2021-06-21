6ae63377a6f96be6b2bdf3ed7e1b98e8d0c0dc42
Vlad Krinitskiy <vlkrinitskiy@gmail.com>
2021-06-21 16:00:05 +0300

    Add identity context

---
+++ src/BugTracker.API/Data/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using BugTracker.API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.API.Data
+{
+    public class ApplicationContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> opts)
+            :base(opts) { }
+    }
+}
--- src/BugTracker.API/Startup.cs
+++ src/BugTracker.API/Startup.cs
@@ -1,6 +1,11 @@
+using BugTracker.API.Data;
 using BugTracker.API.Extensions;
+using BugTracker.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,9 +13,24 @@ namespace BugTracker.API
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<ApplicationContext>(opts =>
+            {
+                opts.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationContext>();
+
             services.AddSwagger();
         }
 
