5431bb6e05dff01b6228a09d5a6d0598d1b96c1e
Vlad Krinitskiy <vlkrinitskiy@gmail.com>
2021-06-26 00:16:19 +0300

    Add axios interceptors to update tokens

--- src/BugTracker.Client/src/services/authService.js
+++ src/BugTracker.Client/src/services/authService.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { api, clearAuthorizationHeader, setAuthorizationHeader } from "../api/api";
 import { clearToken, decodeToken, getToken, saveToken } from "./tokenService";
 
@@ -5,11 +6,9 @@ export function login(username, password) {
   return api.post('account/token', {
     username, password
   }).then(res => {
-
     saveToken(res.data);
     setAuthorizationHeader(res.data.accessToken);
     const user = decodeToken(res.data.accessToken);
-
     return {
       user: user,
       succeeded: true
@@ -20,18 +19,40 @@ export function login(username, password) {
     return {
       user: null,
       succeeded: false,
-      error: error.response.data.error
+      error: error.response?.data?.error
     };
   })
 }
 
-export function loadUser(){
+export function loadUser() {
+  setRefreshInterceptor();
   const token = getToken();
-  if (token){
+  if (token) {
     setAuthorizationHeader(token.accessToken);
     return decodeToken(token.accessToken);
   }
   return null;
 }
 
+export function setRefreshInterceptor() {
+  const interceptor = api.interceptors.response.use(res => res, error => {
+    if (error.response?.status !== 401) {
+      return Promise.reject(error);
+    }
+
+    api.interceptors.response.eject(interceptor);
+
+    const token = getToken();
+    return api.post('account/refresh', {
+      expiredToken: token.accessToken,
+      refreshToken: token.refreshToken
+    }).then(response => {
+      saveToken(response.data);
+      setAuthorizationHeader(response.data.accessToken);
+      return axios(error.response.config);
+    }).catch(error => {
+      clearToken();
+    }).finally(setRefreshInterceptor);
+  });
+}
 
