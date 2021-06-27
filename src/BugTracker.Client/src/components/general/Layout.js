07a61dc9b4265a29c2ba3640ee518c4836941058
Vlad Krinitskiy <vlkrinitskiy@gmail.com>
2021-06-27 20:04:11 +0300

    Add toggle sidebar

--- src/BugTracker.Client/src/components/general/Layout.js
+++ src/BugTracker.Client/src/components/general/Layout.js
@@ -3,25 +3,37 @@ import { Sidebar } from "./Sidebar";
 import { Link } from "react-router-dom";
 import { NavDropdown } from "react-bootstrap";
 import "./Layout.scss";
+import { useState } from "react";
 
 export const Layout = ({ children }) => {
-    const auth = useAuth();
-    return (
-        <div className="d-flex">
-            <Sidebar role={auth.user.role} />
-            <div className="wrapper">
-                <header className="header">
-                    <div className="header-user">Logged in as: <span>{auth.user.username}</span></div>
-                    <NavDropdown title={auth.user.username} id="navbarScrollingDropdown">   
-                        <Link to="/profile" className="dropdown-item">Profile</Link>
-                        <NavDropdown.Divider />
-                        <Link to="/account/logout" className="dropdown-item">Logout</Link>
-                    </NavDropdown>
-                </header>
-                <div className="p-3">
-                    {children}
-                </div>
-            </div>
+  const auth = useAuth();
+  const [sidebarHidden, setHidden] = useState(false);
+
+  const toggle = () => {
+    setHidden(!sidebarHidden);
+  }
+
+  return (
+    <>
+      <Sidebar role={auth.user.role} isHide={sidebarHidden} />
+
+      <div className="header">
+        <div className="header-title">
+          <button className="toggle-sidebar" onClick={toggle} style={!sidebarHidden ? {visibility: "hidden"} : null}><i className="bi bi-list"></i></button>
+          <span><i className="bi bi-bug"></i>Bug Tracker</span>
+          <button className="toggle-sidebar" onClick={toggle} style={sidebarHidden ? {visibility: "hidden"} : null}><i className="bi bi-x-lg"></i></button>
         </div>
-    );
+        <div className="header-user">Logged in as: <span>{auth.user.username}</span></div>
+        <NavDropdown title={auth.user.username} id="navbarScrollingDropdown">
+          <Link to="/profile" className="dropdown-item">Profile</Link>
+          <NavDropdown.Divider />
+          <Link to="/account/logout" className="dropdown-item">Logout</Link>
+        </NavDropdown>
+      </div>
+
+      <div className={`wrapper ${sidebarHidden ? "full" : ""}`}>
+        {children}
+      </div>
+    </>
+  );
 }
--- src/BugTracker.Client/src/components/general/Sidebar.js
+++ src/BugTracker.Client/src/components/general/Sidebar.js
@@ -1,10 +1,9 @@
 import { NavLink } from 'react-router-dom';
 import './Sidebar.scss';
 
-export const Sidebar = ({ role }) => {
+export const Sidebar = ({ role, isHide = false }) => {
     return (
-        <nav className="sidebar shadow">
-            <div className="sidebar-header">Bug Tracker</div>
+        <nav className={`sidebar shadow ${isHide ? "hidden" : ""}`}>
             <ul>
                 <li><NavLink activeClassName="active" exact to='/'><i className="bi bi-grid-1x2"></i>Dashboard</NavLink></li>
                 <li><NavLink activeClassName="active" to='/projects'><i className="bi bi-kanban"></i>Projects</NavLink></li>
